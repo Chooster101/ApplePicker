bd0e61b23d03645c951945ccc1ef1535c1ad0b3e
William Choe <chooster101@gmail.com>
2018-09-21 16:52:52 -0400

    Unfinished Prototype
    has some errors.

---
+++ Assets/_Scene_0/AppleTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Use this for initialization
+    void Start () {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {                                               
+        GameObject apple = Instantiate<GameObject>(applePrefab);  
+        apple.transform.position = transform.position;              
+        Invoke("DropApple", secondsBetweenAppleDrops); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);  // Move right                      
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left   
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;  // Change direction              
+        }
+    }
+}
